b7dba01dce0c9f3fe7d5d2b1970a5dbd1d8d5136
mariaperetiatko <mariia.peretiatko@nure.ua>
2019-04-06 22:50:16 +0300

    Add drag and drop functions

--- Classy.Web/Classy.Web.Frontend/src/app/app.component.ts
+++ Classy.Web/Classy.Web.Frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UploadEvent, UploadFile, FileSystemFileEntry, FileSystemDirectoryEntry } from 'ngx-file-drop';
+
 
 @Component({
   selector: 'app-root',
@@ -6,14 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  files: File[] = [];
-  title = 'front';
 
-  handleFileInput(files: FileList) {
-    this.files.splice(0, this.files.length);
-    for (let file in files){
-      this.files.push(files[file]);
-      alert(this.files[file]);
+  public files: UploadFile[] = [];
+
+  public dropped(event: UploadEvent) {
+    this.files = event.files;
+    for (const droppedFile of event.files) {
+
+      if (droppedFile.fileEntry.isFile) {
+        const fileEntry = droppedFile.fileEntry as FileSystemFileEntry;
+        fileEntry.file((file: File) => {
+
+          console.log(droppedFile.relativePath, file);
+
+        });
+      } else {
+        const fileEntry = droppedFile.fileEntry as FileSystemDirectoryEntry;
+        console.log(droppedFile.relativePath, fileEntry);
+      }
     }
-}
+  }
+
+  public fileOver(event){
+    console.log(event);
+  }
+
+  public fileLeave(event){
+    console.log(event);
+  }
 }
