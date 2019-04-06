fca2c6dff40c922337e2e3ac108933472312787a
mariaperetiatko <mariia.peretiatko@nure.ua>
2019-04-06 22:43:03 +0300

    App module update

--- Classy.Web/Classy.Web.Frontend/src/app/app.module.ts
+++ Classy.Web/Classy.Web.Frontend/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { FileDropModule } from 'ngx-file-drop';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    FileDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
