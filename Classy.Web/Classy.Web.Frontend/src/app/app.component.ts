5504559e747b6e28b629204729ddca538bcf5cde
matan4life <yurii.pohuliaiev@nure.ua>
2019-04-06 18:52:19 +0300

    Uploading files

--- Classy.Web/Classy.Web.Frontend/src/app/app.component.ts
+++ Classy.Web/Classy.Web.Frontend/src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  files: File[] = [];
   title = 'front';
+
+  handleFileInput(files: FileList) {
+    this.files.splice(0, this.files.length);
+    for (let file in files){
+      this.files.push(files[file]);
+      alert(this.files[file]);
+    }
+}
 }
