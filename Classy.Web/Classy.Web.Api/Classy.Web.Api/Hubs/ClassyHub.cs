a5f37225ef104919db12b5e81be0e3576dc70692
matan4life <yurii.pohuliaiev@nure.ua>
2019-04-07 20:36:01 +0300

    Initial SignalR release

---
+++ Classy.Web/Classy.Web.Api/Classy.Web.Api/Hubs/ClassyHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Classy.Web.Api.Hubs
+{
+    public class ClassyHub : Hub
+    {
+        public async Task SendTest(string message)
+        {
+            await this.Clients.Caller.SendAsync(message);
+        }
+    }
+}
