6f6bfeeb65e1ddd4f70b6f33a349361f615f3c0e
mariaperetiatko <mariia.peretiatko@nure.ua>
2019-04-09 18:14:25 +0300

    Add folder contents links and index transfer

--- Classy.Web/Classy.Web.Frontend/src/app/folder-contents-page/folder-contents-page.component.ts
+++ Classy.Web/Classy.Web.Frontend/src/app/folder-contents-page/folder-contents-page.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-folder-contents-page',
   templateUrl: './folder-contents-page.component.html',
   styleUrls: ['./folder-contents-page.component.css']
 })
-export class FolderContentsPageComponent implements OnInit {
+export class FolderContentsPageComponent implements OnInit, OnDestroy {
+
+  imageClassIndex: number;
 
   constructor() { }
 
   ngOnInit() {
+    this.imageClassIndex = Number.parseInt(localStorage.getItem('classIndex'), 10);
+  }
+
+  ngOnDestroy() {
+    localStorage.removeItem('classIndex');
   }
 
 }
