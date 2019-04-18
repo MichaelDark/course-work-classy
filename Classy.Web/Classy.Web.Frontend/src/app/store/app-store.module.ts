d1e2d8704bd1507652055474c1856cb0747092d0
Lyubchenko Yuriy <undeadspez@gmail.com>
2019-04-19 02:20:24 +0300

    Add @ngrx/router-store

--- Classy.Web/Classy.Web.Frontend/src/app/store/app-store.module.ts
+++ Classy.Web/Classy.Web.Frontend/src/app/store/app-store.module.ts
@@ -1,10 +1,12 @@
-import { environment } from '../../environments/environment.prod';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { reducers, metaReducers } from './reducers';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+
+import { environment } from '../../environments/environment.prod';
+import { reducers, metaReducers } from './reducers';
 import { ImageEffects } from './effects/image.effects';
 
 const modules = [
@@ -14,7 +16,8 @@ const modules = [
     maxAge: 25,
     logOnly: environment.production,
   }),
-  EffectsModule.forRoot([ImageEffects])
+  EffectsModule.forRoot([ImageEffects]),
+  StoreRouterConnectingModule.forRoot()
 ];
 
 @NgModule({
