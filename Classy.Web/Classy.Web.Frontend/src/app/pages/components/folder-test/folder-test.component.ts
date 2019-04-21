a399f16e59178104cfd36f11e9ae0d505ae33235
maxbe <maxben708@gmail.com>
2019-04-21 18:34:25 +0300

    Moved convert to map

--- Classy.Web/Classy.Web.Frontend/src/app/pages/components/folder-test/folder-test.component.ts
+++ Classy.Web/Classy.Web.Frontend/src/app/pages/components/folder-test/folder-test.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Image } from '@classy/store/models';
 import { Store, select } from '@ngrx/store';
 import * as fromRoot from '@classy/store/reducers';
-import { skip, take } from 'rxjs/operators';
+import { skip, take, map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-folder-test',
@@ -13,30 +14,32 @@ export class FolderTestComponent implements OnInit {
   images$ = this.store.pipe(select(fromRoot.getImagesState))
   min: number = 0;
   max: number = 25;
-  Images: Image[] = [];
+  Images: string[] = [];
   class: string = "test";
-  constructor(private store: Store<fromRoot.State>) { 
-    this.images$.pipe(skip(this.min), take(25)).subscribe(images => this.Images = images);
+  constructor(private store: Store<fromRoot.State>) {
+    let parent = this;
+    this.images$.pipe(skip(this.min), take(25), map<Image[], string[]>(x => x.map(parent.convert))).subscribe(images => this.Images = images);
   }
 
   ngOnInit() {
   }
 
-  increment(){
-    if (this.min < this.Images.length){
-      this.min +=5;
+  increment() {
+    if (this.min < this.Images.length) {
+      this.min += 5;
     }
   }
 
-  decrement(){
-    if (this.min >0){
-      this.min -=5;
+  decrement() {
+    if (this.min > 0) {
+      this.min -= 5;
     }
   }
 
-  public convert(image: Image){
+  public convert(image: Image) {
     let reader = new FileReader();
     reader.readAsDataURL(image.file);
+    console.log("State: " + reader.readyState);
     return reader.result.toString();
   }
 }
