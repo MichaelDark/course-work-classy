781e26fd64bfaa9ca839cb945a06784b24544031
Lyubchenko Yuriy <undeadspez@gmail.com>
2019-04-21 22:33:04 +0300

    Inject store

--- Classy.Web/Classy.Web.Frontend/src/app/pages/components/folders-list/folders-list.component.ts
+++ Classy.Web/Classy.Web.Frontend/src/app/pages/components/folders-list/folders-list.component.ts
@@ -1,46 +1,45 @@
-import { ClassificationStorageService, storageFactory } from '@classy/core/services/classification-storage.service';
 import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import { ClassificationStorageService, storageFactory } from '@classy/core/services/classification-storage.service';
 import { FileClass } from '@classy/store/models';
 import { Store, select } from '@ngrx/store';
 import * as fromRoot from '@classy/store/reducers';
-import { skip, take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-folders-list',
   templateUrl: './folders-list.component.html',
   styleUrls: ['./folders-list.component.css']
 })
-export class FoldersListComponent implements OnInit, OnChanges {
-
-  storage: Storage;
+export class FoldersListComponent {
 
   ImageClasses: string[] = [];
 
-  constructor( ) { }
+  images$ = this.store.pipe(select(fromRoot.getImagesState));
+
+  constructor(
+    private store: Store<fromRoot.State>
+  ) { }
 
   ngOnInit() {
     //this.storage = localStorage;
     //localStorage.clear();
     this.defineClasses();
-
   }
+
   ngOnChanges(changes: SimpleChanges) {
     this.defineClasses();
   }
 
   defineClasses(): void {
-     const imageStrings = localStorage.getItem('classy');
-     const imageObjects = JSON.parse(imageStrings);
-     console.log(imageObjects);
+    const imageStrings = localStorage.getItem('classy');
+    const imageObjects = JSON.parse(imageStrings);
+    console.log(imageObjects);
 
-     for (const key in imageObjects) {
-        if (!this.ImageClasses.includes(imageObjects[key])) {
-          this.ImageClasses.push(imageObjects[key]);
-
+    for (const key in imageObjects) {
+      if (!this.ImageClasses.includes(imageObjects[key])) {
+        this.ImageClasses.push(imageObjects[key]);
       }
-     }
-     console.log(this.ImageClasses);
-
+    }
+    console.log(this.ImageClasses);
   }
 
 }
