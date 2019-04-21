901e6d9f07abcb494afcd79cbef5dd84ccb25ced
Mykhailo Temnokhud <michael_dark@list.ru>
2019-04-21 22:02:47 +0300

    possible fix for memory leaks

--- Classy.Mobile/lib/main.dart
+++ Classy.Mobile/lib/main.dart
@@ -20,6 +20,8 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
+    imageCache.maximumSize = 25;
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: Strings.of(context).appName,
