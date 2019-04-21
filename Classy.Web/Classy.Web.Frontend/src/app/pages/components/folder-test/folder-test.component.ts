3aafe219c8caefa0cbb92c5fd81b0de1e0f1144d
matan4life <yurii.pohuliaiev@nure.ua>
2019-04-21 21:43:25 +0300

    Modal window

--- Classy.Web/Classy.Web.Frontend/src/app/pages/components/folder-test/folder-test.component.ts
+++ Classy.Web/Classy.Web.Frontend/src/app/pages/components/folder-test/folder-test.component.ts
@@ -4,6 +4,7 @@ import { Store, select } from '@ngrx/store';
 import * as fromRoot from '@classy/store/reducers';
 import { skip, take, map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-folder-test',
@@ -15,10 +16,45 @@ export class FolderTestComponent implements OnInit {
   min: number = 0;
   max: number = 25;
   Images: string[] = [];
+  ImageFileNames: string[] = [];
   class: string = "test";
-  constructor(private store: Store<fromRoot.State>) {
+  closeResult: string;
+  constructor(private store: Store<fromRoot.State>, private modalService: NgbModal) {
     let parent = this;
-    this.images$.pipe(skip(this.min), take(25), map<Image[], string[]>(x => x.map(parent.convert))).subscribe(images => this.Images = images);
+    this.images$.pipe(skip(this.min), take(25)).subscribe(images => {
+      for (let img of images) {
+        if (!parent.ImageFileNames.includes(img.file.name)) {
+          parent.ImageFileNames.push(img.file.name);
+        }
+        else {
+          continue;
+        }
+
+        let reader = parent.startConvert(img);
+        reader.addEventListener('loadend', () => {
+          let result = reader.result.toString();
+          parent.Images.push(result);
+        });
+      }
+    });
+  }
+
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
   }
 
   ngOnInit() {
@@ -36,10 +72,9 @@ export class FolderTestComponent implements OnInit {
     }
   }
 
-  public convert(image: Image) {
+  public startConvert(image: Image) {
     let reader = new FileReader();
     reader.readAsDataURL(image.file);
-    console.log("State: " + reader.readyState);
-    return reader.result.toString();
+    return reader;
   }
 }
