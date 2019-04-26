3b82b79accc57577cfe5f6fa9995f7578e45856d
Lyubchenko Yuriy <undeadspez@gmail.com>
2019-04-27 02:19:14 +0300

    Remove unused dependencies

---
+++ Classy.Web/Classy.Web.Frontend/src/app/store/effects/index.ts
@@ -0,0 +1,9 @@
+import { ImageEffects } from './image.effects';
+import { UserEffects } from './user.effects';
+import { LayoutEffects } from './layout.effects';
+
+export const effects = [
+  ImageEffects,
+  UserEffects,
+  LayoutEffects
+];
