5732fd87c947769f9ea452ce31be821ba2ae6538
matan4life <yurii.pohuliaiev@nure.ua>
2019-04-29 20:19:43 +0300

    Initial reclassify

--- Classy.Web/Classy.Web.Frontend/src/app/pages/components/folder-contents/folder-contents.component.ts
+++ Classy.Web/Classy.Web.Frontend/src/app/pages/components/folder-contents/folder-contents.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { select, Store } from '@ngrx/store';
 import * as fromRoot from '@classy/store/reducers';
+import { Image } from '@classy/store/models';
+import { map } from 'rxjs/operators';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-folder-contents',
@@ -9,20 +12,54 @@ import * as fromRoot from '@classy/store/reducers';
   styleUrls: ['./folder-contents.component.css']
 })
 export class FolderContentsComponent {
-
+  closeResult: string;
   class: string;
+  currentImage: Image;
 
   images$ = this.store.pipe(select(fromRoot.getImagesState));
+  images: Image[] = [];
 
   constructor(
     private route: ActivatedRoute,
-    private store: Store<fromRoot.State>
-  ) { }
-
-  ngOnInit() {
+    private store: Store<fromRoot.State>,
+    private modalService: NgbModal
+  ) {
     this.route.paramMap.subscribe(params => {
       this.class = params.get("class");
     });
+  }
+
+  ngOnInit() {
+    this.images$.pipe(
+      map(images => images.filter(i => i.class == this.class))
+    ).subscribe(images => {
+      this.images = images;
+    })
+  }
+
+  openModal(content, image: Image){
+    this.currentImage = image;
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
+
+  Save(){
+    let newClass = document.getElementById('myInput').innerText;
+    this.currentImage.class = newClass;
+    this.ngOnInit();
   }
 
 }
