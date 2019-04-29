f224de7e255f59ea3b3d48e914492f31b407a142
Lyubchenko Yuriy <undeadspez@gmail.com>
2019-04-30 00:49:18 +0300

    Close modal after reclassification

--- Classy.Web/Classy.Web.Frontend/src/app/pages/components/folder-contents/folder-contents.component.ts
+++ Classy.Web/Classy.Web.Frontend/src/app/pages/components/folder-contents/folder-contents.component.ts
@@ -3,8 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import { select, Store } from '@ngrx/store';
 import * as fromRoot from '@classy/store/reducers';
 import { Image } from '@classy/store/models';
-import { map, debounceTime, distinctUntilChanged, distinct, switchMap } from 'rxjs/operators';
-import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { map, debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { NgbModal, ModalDismissReasons, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { ImageActions } from '@classy/store/actions';
 import { Observable } from 'rxjs';
 
@@ -14,14 +14,17 @@ import { Observable } from 'rxjs';
   styleUrls: ['./folder-contents.component.css']
 })
 export class FolderContentsComponent {
+
   closeResult: string;
-  class: string;
+  currentFolder: string;
   currentImage: Image;
-  newClassModel: string;
+  newClass: string;
 
   images$ = this.store.pipe(select(fromRoot.getImagesState));
   classes$: Observable<string[]>
   images: Image[] = [];
+
+  modalRef: NgbModalRef;
 
   constructor(
     private route: ActivatedRoute,
@@ -29,53 +32,58 @@ export class FolderContentsComponent {
     private modalService: NgbModal
   ) {
     this.route.paramMap.subscribe(params => {
-      this.class = params.get("class");
+      this.currentFolder = params.get("class");
     });
   }
 
   ngOnInit() {
     this.images$.pipe(
-      map(images => images.filter(i => i.class == this.class))
-    ).subscribe(images => {
-      this.images = images;
-    });
+      map(images =>
+        images.filter(i => i.class == this.currentFolder))
+    ).subscribe(images => this.images = images);
 
-    this.classes$ = this.images$.pipe(map(images => images.map(x => x.class)
-      .filter((val, idx, arr) => arr.indexOf(val) === idx))); // === distinct
+    this.classes$ = this.images$.pipe(
+      map(images => images.map(x => x.class).filter(
+        (val, idx, arr) => arr.indexOf(val) === idx)
+      )
+    ); // === distinct
   }
 
   openModal(content, image: Image) {
     this.currentImage = image;
-    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
+    this.modalRef = this.modalService.open(content);
   }
 
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return `with: ${reason}`;
-    }
-  }
+  // private getDismissReason(reason: any): string {
+  //   if (reason === ModalDismissReasons.ESC) {
+  //     return 'by pressing ESC';
+  //   } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+  //     return 'by clicking on a backdrop';
+  //   } else {
+  //     return `with: ${reason}`;
+  //   }
+  // }
 
   save() {
-    let newClass = this.newClassModel;
-    this.store.dispatch(ImageActions.reclassify({ fileName: this.currentImage.file.name, newClass }));
-    // this.currentImage.class = newClass;
-    // this.ngOnInit();
+    this.modalRef.close();
+    this.store.dispatch(ImageActions.reclassify({
+      fileName: this.currentImage.file.name,
+      newClass: this.newClass
+    }));
   }
 
   public search = (text$: Observable<string>) =>
     text$.pipe(
       debounceTime(200),
       distinctUntilChanged(),
-      switchMap(term => term.length < 2 ? []
-        : this.classes$.pipe(map(classes => classes.filter(
-          val => (val.toLowerCase().indexOf(term.toLowerCase()) > -1)
-        ).slice(0, 10)))));
+      switchMap(term => term.length < 2
+        ? []
+        : this.classes$.pipe(
+          map(classes =>
+            classes.filter(
+              val => (val.toLowerCase().indexOf(term.toLowerCase()) > -1)
+            ).slice(0, 10))
+          )
+      )
+    );
 }
